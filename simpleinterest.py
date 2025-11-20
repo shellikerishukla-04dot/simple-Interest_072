217689d1eabd1c8821f8c8677515adeca750d418
shellikerishukla-04dot <shellikerishukla@gmail.com>
2025-11-20 15:33:51 +0530

    Update simpleinterest.py

--- simpleinterest.py
+++ simpleinterest.py
@@ -1,21 +1,21 @@
 import sys
 
 if len(sys.argv) == 4:
-    principal = float(sys.argv[1])
-    rate = float(sys.argv[2])
-    time = float(sys.argv[3])
+    principal =sys.argv[1])
+    rate =sys.argv[2])
+    time =sys.argv[3])
     print("user provided input values:")
 else:
-    print("No (or not enough) input given - using default values")
-    principal = 1000.0
-    rate = 5.0
-    time = 2.0
+    print("No input given - using default values:")
+    principal = "1000"
+    rate = "5"
+    time = "2"
 
 # Calculate simple interest
-simple_interest = (principal * rate * time) / 100
+simple_interest = (eval(principal) * eval(rate) * eval(time)) / 100
 
 # Print results
 print("Principal Amount:", principal)
-print("Rate of Interest (% per annum):", rate)
+print("Rate of Interest:", rate)
 print("Time Period (years):", time)
 print("Simple Interest =", simple_interest)
